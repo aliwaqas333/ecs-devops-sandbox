55577c2f45f83cf2839788fe1c41859ac113847d
Ali Waqas <aliwaqas@codeforpakistan.org>
2021-04-21 09:42:14 +0200

    pass test

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-"""Main application file"""
+"""
+Main application file
+"""
 from flask import Flask
+import logging
 app = Flask(__name__)
+
+# Initialize Logger
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
-    """Reverse and return the provided URI LOL"""
+    """Reverse and return the provided URI"""
+    LOGGER.info('Received a message: %s', random_string)
     return "".join(reversed(random_string))
 
 if __name__ == '__main__':
